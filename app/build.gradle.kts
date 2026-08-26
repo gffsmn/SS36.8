f13a6d6732a77ed58823ffeb90855aa64fb4e6a4
gffsmn <121425062+gffsmn@users.noreply.github.com>
2026-08-26 09:10:48 +0200

    Update versionCode and versionName to 19.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.ss36monitor"
     minSdk = 24
     targetSdk = 36
-    versionCode = 18
-    versionName = "18.0"
+    versionCode = 19
+    versionName = "19.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
